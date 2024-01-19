682a75125d37d7cd17a337f7c4bb989fff39c82b
Michael Shyu <michaelshyu11@gmail.com>
2024-01-19 15:20:39 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    let result1 = record.find(function(element) {
+        return element.result === "W";
+    });
+    return result1 ? result1.year : undefined;
+}
+
